e1bbd0a3b61cba0a824b8e033e0fde15d6aeccd7
grim-ops <grim-ops@protonmail.com>
2020-11-17 21:05:26 -0600

    Initial commit

--- src/bin_conv.c
+++ src/bin_conv.c
@@ -10,13 +10,11 @@ void str_to_bin(char *file_str, size_t size_of_file, unsigned char *bin_arr)
 	for(int ind = 0; ind < size_of_file; ind++)
 	{
 		char byte = file_str[ind];
-		printf("\n%c = ", byte);
 		for(int i = 0; i < 8; i++)
 		{
 			char temp = byte >> (i * -1) + (8 - 1);
 			if(temp % 2)
 				bin_arr[count] = 1;
-			printf("%d ", temp % 2);
 			count++;
 		}
 	}
@@ -33,12 +31,29 @@ void bin_to_str(unsigned char *bin_arr, size_t size_of_bin_arr, char *file_str)
 			file_str[i] = file_str[i] << 1;
 			if(bin_arr[n] == 1){
 				file_str[i]++;
-				printf("1");
 			}
 			else{
-				printf("0");
 			}
 		}
-	printf("%c\n", file_str[i]);
+	}
+}
+
+void int64_to_bin(size_t integer_64, unsigned char *bin_arr)
+{
+
+	for(int i = 63; i >= 0; i--)
+	{
+		bin_arr[i] = integer_64 % 2;
+		integer_64 = integer_64 >> 1;
+	}
+}
+
+void bin_to_int64(size_t *integer_64, unsigned char *bin_arr)
+{
+	*integer_64 = 0;
+	for(int i = 0; i < 64; i++)
+	{
+		*integer_64 = *integer_64 << 1;
+		*integer_64 += bin_arr[i];
 	}
 }
--- src/bin_conv.h
+++ src/bin_conv.h
@@ -4,5 +4,7 @@
 
 void str_to_bin(char *file_str, size_t size_of_file, unsigned char *bin_arr);
 void bin_to_str(unsigned char *bin_arr, size_t size_of_bin_arr, char *file_str);
+void int64_to_bin(size_t integer_64, unsigned char *bin_arr);
+void bin_to_int64(size_t *integer_64, unsigned char *bin_arr);
 
 #endif
